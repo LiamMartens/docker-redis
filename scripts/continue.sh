dee1723fb5e47e994c64ea50cce947d8f3b69d33
Liam Martens <liam@Liams-MacBook-Pro.local>
2017-04-29 12:51:13 +0200

    Function fix bash ()

--- scripts/continue.sh
+++ scripts/continue.sh
@@ -35,7 +35,7 @@ for VAR in "${ENV_VARS[@]}"; do
 		REDIS_SETTING=$(echo $REDIS_SETTING | awk '{print tolower($0)}')
 		REDIS_SETTING=$(echo $REDIS_SETTING | perl -pe "s/_/-/")
 		perl -p -i.bak -e "s/^$REDIS_SETTING\s+.*/$REDIS_SETTING $VAR_VALUE/gi" /etc/redis/redis.conf
-		checkRedisConf()
+		checkRedisConf
 	fi
 done
 
@@ -45,11 +45,11 @@ echo $TIMEZONE > /etc/timezone
 
 # set redis listen port
 perl -p -i.bak -e "s/bind\s+(.+)/bind $REDIS_TCP/gi" /etc/redis/redis.conf
-checkRedisConf()
+checkRedisConf
 
 # set redis listen port
 perl -p -i.bak -e "s/port\s+(.+)/port $REDIS_PORT/gi" /etc/redis/redis.conf
-checkRedisConf()
+checkRedisConf
 
 echo "Starting redis on $REDIS_PORT"
 redis-server /etc/redis/redis.conf --loglevel verbose
